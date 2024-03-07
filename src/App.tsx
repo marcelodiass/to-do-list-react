d7ee728c41ac2342848f8288d3af3e22a180f792
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-03-07 17:30:08 -0300

    feat: add TodoItem and delete TodoItem.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
-import TodoItem from "./components/TodoItem"
+import { useState } from "react"
+import AddTodoForm from "./components/AddTodoForm"
+import TodoList from "./components/TodoList"
 import { dummyData } from "./data/todos"
 
 function App() {
+  const [todos, setTodos] = useState(dummyData)
+
+  function setTodoCompleted(id: number, completed: boolean) {
+    setTodos((prevTodos) => prevTodos.map(todo => (
+      todo.id === id ? { ...todo, completed } : todo
+    )))
+  }
+
+  function addTodo(title: string) {
+    setTodos(prevTodos => [
+      {
+        id: prevTodos.length + 1,
+        title,
+        completed: false
+      },
+      ...prevTodos
+    ])
+  }
+
+  function deleteTodo(id:number) {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
+  }
 
   return (
-    <main className="py-10 h-screen space-y-5">
+    <main className="py-10 h-screen space-y-5 overflow-y-auto">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
-      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
-        <div className="space-y-2">
-          {dummyData.map(todo => (
-            <TodoItem todo={todo}/>
-          ))}
-        </div>
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
+        <AddTodoForm onSubmit={addTodo}/>
+        <TodoList todos={todos} onCompletedChange={setTodoCompleted} onDelete={deleteTodo}/>
       </div>
     </main>
   )
