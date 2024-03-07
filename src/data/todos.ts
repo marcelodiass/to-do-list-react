f9bdcbd0ec618b2effdecbce9b2fbd1e5586e30f
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-03-06 23:10:31 -0300

    feat: Todo Items and todo type declaration.

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: 'Todo 1',
+        completed: false
+    },
+    {
+        id: 2,
+        title: 'Todo 2',
+        completed: false
+    },
+    {
+        id: 3,
+        title: 'Todo 3',
+        completed: false
+    }
+]
